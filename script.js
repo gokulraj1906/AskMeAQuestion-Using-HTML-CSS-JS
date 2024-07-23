4227daf5bb3f7b85a94f5c051971e1090f67169a
gokulraj1906 <141732593+gokulraj1906@users.noreply.github.com>
2024-07-23 17:22:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let arr=[];
+function Accept(){
+    let str=document.getElementById("input");
+    arr.push(str);
+    document.getElementById("op").innerHTML="Thanks for your response";
+}
+function Decline(){
+    window.close();
+}
+
+    
